ba4721aa9e37367d216a0f589493fafe18f7d427
Ayshasi <161432267+Ayshasi@users.noreply.github.com>
2026-02-03 12:14:06 +0530

    Add files via upload

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegExApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = @"D:\Alt-Text_Meta_GSLSpecPub2023-112";
+            string pattern = @"^[a-zA-Z0-9_\-:\\\.<]+"; // allows valid path chars
+
+            Regex rgx = new Regex(pattern);
+            bool isValid = rgx.IsMatch(input);
+
+            if (isValid)
+            {
+                Console.WriteLine("The input address is valid.");
+            }
+            else
+            {
+                Console.WriteLine("The input address is invalid.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

dfd3f0f20b84c95f153bda3835b02ed44388b84f
Ayshasi <161432267+Ayshasi@users.noreply.github.com>
2026-02-03 12:24:50 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class1
+    {
+       public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+        {
+
+            //Console.WriteLine($"Yearly Wage: {monthlyWage * numberOfMonthsWorked}");
+            //return monthlyWage * numberOfMonthsWorked;
+
+            if (numberOfMonthsWorked == 12)
+                return monthlyWage * (numberOfMonthsWorked + 1);
+
+            return monthlyWage * numberOfMonthsWorked;
+        }
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked,int bonus)
+        {
+            Console.WriteLine($"Yearly Wage with bonus: {monthlyWage * numberOfMonthsWorked + bonus}");
+
+            return monthlyWage * numberOfMonthsWorked + bonus ;
+        }
+
+        public static double CalculateYearlyWage(double monthlyWage, double numberOfMonthsWorked, double bonus)
+        {
+            Console.WriteLine($"Yearly Wage (double): {monthlyWage * numberOfMonthsWorked + bonus}");
+            return monthlyWage * numberOfMonthsWorked + bonus;
+
+
+        }
+    }
+}
---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Create a list of numbers
+        List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // LINQ Method Syntax → filter even numbers
+        var evenNumbers = numbers.Where(n => n % 2 == 0);
+
+        Console.WriteLine("Even Numbers:");
+        foreach (var num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+0l̥

6ec8c6e42ec7ff1b967795a1e28f0de34b88722c
Ayshasi <161432267+Ayshasi@users.noreply.github.com>
2026-02-03 12:06:50 +0530

    Add files via upload

---
+++ Abstract&Interface/Abstract&Interface/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// interface 
+public interface Student
+{
+    //abstract method
+    void FavSub();
+}
+
+// Subclass for functionality
+public class Abc : Student
+{
+    public void FavSub()
+    {
+        Console.WriteLine("Hello! How are you? ");
+    }
+}
+
+
+public class Geeks
+{
+    public static void Main()
+    {
+        Student obj = new Abc();
+
+        obj.FavSub();
+    }
+}
